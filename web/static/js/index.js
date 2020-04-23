ec71529ed0325c3c4e92759b45a2a945e244efeb
grvela <rodrigoandre777@gmail.com>
2020-04-23 12:55:41 -0400

    mobile chat

--- web/static/js/index.js
+++
@@ -1,9 +0,0 @@
-let menu = document.querySelector("#menu");
-
-menu.addEventListener("click", function(){
-    let showMenu = document.querySelector(".navbar").style.display;
-    if(showMenu == 'none')
-        document.querySelector(".navbar").style.display = 'block';
-    else
-        document.querySelector(".navbar").style.display = 'none';
-});
